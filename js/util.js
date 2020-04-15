93ca5eb5d64827b275627c30003335229aa980e5
IvanLuLyf <835498692@qq.com>
2020-04-15 23:00:42 +0800

    更新通用工具

--- js/util.js
+++ js/util.js
@@ -10,6 +10,96 @@
         $(el).html(html);
     }
 
+    function Dialog(id, conf) {
+        conf = conf || {};
+        var $dialog = $('#dialog_' + id);
+        if ($dialog.length === 0) {
+            $dialog = $('<div class="modal" id="dialog_' + id + '" tabindex="-1"' + (conf.staticBack ? ' data-backdrop="static"' : '') + ' role="dialog"></div>')
+            $('body').append($dialog);
+        }
+        $dialog.html('<div class="modal-dialog' + (conf.center ? ' modal-dialog-centered' : '') + '" role="document"><div class="modal-content"></div></div>');
+        var $content = $dialog.find('.modal-content');
+        if (conf.header) {
+            var header = '<div class="modal-header"><h5 class="modal-title">' + (conf.header.title || '') + '</h5>';
+            if (conf.header.close) {
+                header += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+            }
+            header += '</div>';
+            $content.append(header);
+        }
+        this.$body = $('<div class="modal-body"></div>');
+        if (conf.body) {
+            this.$body.html(conf.body);
+        }
+        $content.append(this.$body);
+        if (conf.ok !== undefined || conf.cancel !== undefined) {
+            var footer = '<div class="modal-footer">';
+            if (conf.ok !== undefined) {
+                footer += '<button type="button" class="btn btn-primary">' + conf.ok + '</button>';
+            }
+            if (conf.cancel !== undefined) {
+                footer += '<button type="button" class="btn btn-secondary" data-dismiss="modal">' + conf.cancel + '</button>';
+            }
+            footer += '</div>';
+            $content.append(footer);
+        }
+        this.$dialog = $dialog;
+    }
+
+    Dialog.prototype.body = function () {
+        return this.$body;
+    };
+
+    Dialog.prototype.show = function () {
+        this.$dialog.modal('show');
+    };
+
+    Dialog.prototype.hide = function () {
+        this.$dialog.modal('hide');
+        this.$dialog.hide();
+    };
+
+    function createDialog(id, conf) {
+        return new Dialog(id, conf);
+    }
+
+    function tipBox(msg) {
+        var dialog = new Dialog('tip', {
+            body: msg,
+        });
+        dialog.show();
+        return dialog;
+    }
+
+    function messageBox(title, msg) {
+        var dialog = new Dialog('msg', {
+            header: {
+                title: title,
+                close: true,
+            },
+            body: msg,
+            ok: '确定',
+            cancel: '取消',
+        });
+        dialog.show();
+        return dialog;
+    }
+
+    function loadingBox(msg, color) {
+        color = color || 'primary';
+        var html = '<div class="text-center mt-3"><div class="spinner-border text-' + color + '" role="status"><span class="sr-only">Loading...</span></div></div>';
+        if (msg) {
+            html += '<div class="text-center mt-3">' + msg + '</div>';
+        }
+        var dialog = new Dialog('loading', {
+            staticBack: true,
+            center: true,
+            body: html,
+        });
+        dialog.show();
+        return dialog;
+    }
+
     function checkLogin() {
         if ($.Eira.storage('user').token) {
             return true;
@@ -38,6 +128,10 @@
 
     W.Utils = {
         showAlert: showAlert,
+        messageBox: messageBox,
+        loadingBox: loadingBox,
+        tipBox: tipBox,
+        createDialog: createDialog,
         checkLogin: checkLogin,
         apiPost: apiPost,
         logout: logout,

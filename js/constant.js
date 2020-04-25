7c2768a806d8d3650fdb874a657de2e1bdf5e8e6
IvanLuLyf <835498692@qq.com>
2020-04-25 23:53:13 +0800

    新增常量文件

---
+++ js/constant.js
@@ -0,0 +1,9 @@
+(function (WIN) {
+    var USER_ROLES = {
+        0: '<span class="badge badge-dark">普通用户</span>',
+        1: '<span class="badge badge-primary">管理员</span>',
+    };
+    WIN.Constant = {
+        USER_ROLES: USER_ROLES,
+    }
+})(window);
